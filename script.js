0e3b621e783958ca6af47f6064a9ecf2483329e2
StathisDrim <asosproject@outlook.com>
2024-11-07 11:10:28 +0200

    Modified index and CSS styles

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    $('#book').turn({
-        width: 800,
-        height: 600,
-        autoCenter: true
-    });
+$(function(){
+	var book = $('#book');
+	$('#view-cover').click(function(){
+		$(this).addClass('cur').siblings().removeClass('cur');
+		book.removeClass().addClass('view-cover');
+	});
+	$('#view-back').click(function(){
+		$(this).addClass('cur').siblings().removeClass('cur');
+		book.removeClass().addClass('view-back');
+	});
+	$('#open-book').click(function(){
+		if ( book.attr('class') !='open-book') {
+			$(this).addClass('cur').siblings().removeClass('cur');
+			book.removeClass().addClass('open-book');
+		}else{
+			$(this).removeClass('cur');
+			$('#view-cover').addClass('cur');
+			book.removeClass().addClass('view-cover');
+		}
+	});
+	$('#view-rotate').click(function(){
+		$(this).addClass('cur').siblings().removeClass('cur');
+		book.removeClass().addClass('view-rotate');
+	});
 });
