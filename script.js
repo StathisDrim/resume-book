47cbde7cbd1ebd4e273d13ad37d1178fe0d9eb24
StathisDrim <asosproject@outlook.com>
2024-11-08 10:50:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,46 +2,43 @@ $(function() {
     console.log("Το script.js φορτώθηκε σωστά!");
 
     var book = $('#book');
-    
-    // Λίστα των καταστάσεων για εναλλαγή
+
+    // Καταστάσεις και κουμπιά που τις ελέγχουν
     var states = ["view-cover", "view-back", "open-book", "view-rotate"];
-    var currentState = 0;  // Ένδειξη τρέχουσας κατάστασης
-    
-    // Κλικ στα κουμπιά της κονσόλας
+    var currentState = 0; // Ένδειξη της τρέχουσας κατάστασης
+
+    // Συνάρτηση για αλλαγή της κατάστασης του βιβλίου
+    function changeBookState(newStateIndex) {
+        currentState = newStateIndex;
+        book.removeClass().addClass(states[currentState]);
+        // Ενημέρωση των κουμπιών
+        $('#opt dd').removeClass('cur').eq(currentState).addClass('cur');
+    }
+
+    // Εναλλαγή με τα κουμπιά της κονσόλας
     $('#view-cover').click(function() {
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-cover');
-        currentState = 0;
+        changeBookState(0);
     });
-    
+
     $('#view-back').click(function() {
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-back');
-        currentState = 1;
+        changeBookState(1);
     });
-    
+
     $('#open-book').click(function() {
-        if (book.attr('class') != 'open-book') {
-            $(this).addClass('cur').siblings().removeClass('cur');
-            book.removeClass().addClass('open-book');
-            currentState = 2;
+        if (book.hasClass('open-book')) {
+            changeBookState(0); // Κλείνει το βιβλίο και γυρνάει στο εξώφυλλο
         } else {
-            $(this).removeClass('cur');
-            $('#view-cover').addClass('cur');
-            book.removeClass().addClass('view-cover');
-            currentState = 0;
+            changeBookState(2); // Ανοίγει το βιβλίο
         }
     });
-    
+
     $('#view-rotate').click(function() {
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-rotate');
-        currentState = 3;
+        changeBookState(3);
     });
-    
+
     // Κλικ στο βιβλίο για εναλλαγή καταστάσεων
     book.click(function() {
-        currentState = (currentState + 1) % states.length; // Επόμενη κατάσταση
-        book.removeClass().addClass(states[currentState]);  // Αλλαγή κατάστασης
+        currentState = (currentState + 1) % states.length; // Εναλλαγή στην επόμενη κατάσταση
+        changeBookState(currentState);  // Αλλαγή κατάστασης και ενημέρωση κουμπιών
     });
 });
