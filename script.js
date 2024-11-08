290b65e7d9771c39971253adf254023e7504d97f
StathisDrim <asosproject@outlook.com>
2024-11-08 11:40:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
 $(function() {
     console.log("Το script.js φορτώθηκε σωστά!");
+
     var book = $('#book');
-    var pages = $('.page'); // Βρίσκουμε όλες τις σελίδες
 
-    var currentPage = 0; // Αρχική σελίδα
+    // Καταστάσεις και κουμπιά που τις ελέγχουν
+    var states = ["view-cover", "view-back", "open-book", "view-rotate"];
+    var currentState = 0; // Ένδειξη της τρέχουσας κατάστασης
 
-    // Εμφάνιση πρώτης σελίδας
-    $(pages[currentPage]).addClass('active');
+    // Συνάρτηση για αλλαγή της κατάστασης του βιβλίου
+    function changeBookState(newStateIndex) {
+        currentState = newStateIndex;
+        book.removeClass().addClass(states[currentState]);
+        // Ενημέρωση των κουμπιών
+        $('#opt dd').removeClass('cur').eq(currentState).addClass('cur');
+    }
 
-    // Επόμενη σελίδα
-    $('#next-page').click(function() {
-        if (currentPage < pages.length - 1) {
-            $(pages[currentPage]).removeClass('active');
-            currentPage++;
-            $(pages[currentPage]).addClass('active');
-        }
+    // Εναλλαγή με τα κουμπιά της κονσόλας
+    $('#view-cover').click(function() {
+        changeBookState(0);
     });
 
-    // Προηγούμενη σελίδα
-    $('#prev-page').click(function() {
-        if (currentPage > 0) {
-            $(pages[currentPage]).removeClass('active');
-            currentPage--;
-            $(pages[currentPage]).addClass('active');
-        }
+    $('#view-back').click(function() {
+        changeBookState(1);
     });
 
-    // Άλλες ενέργειες για την κονσόλα (View Cover, Rotate, Open/Close, κλπ.)
-    $('#view-cover').click(function(){
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-cover');
-    });
-
-    $('#view-back').click(function(){
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-back');
-    });
-
-    $('#open-book').click(function(){
-        if (book.attr('class') != 'open-book') {
-            $(this).addClass('cur').siblings().removeClass('cur');
-            book.removeClass().addClass('open-book');
+    $('#open-book').click(function() {
+        if (book.hasClass('open-book')) {
+            changeBookState(0); // Κλείνει το βιβλίο και γυρνάει στο εξώφυλλο
         } else {
-            $(this).removeClass('cur');
-            $('#view-cover').addClass('cur');
-            book.removeClass().addClass('view-cover');
+            changeBookState(2); // Ανοίγει το βιβλίο
         }
     });
 
-    $('#view-rotate').click(function(){
-        $(this).addClass('cur').siblings().removeClass('cur');
-        book.removeClass().addClass('view-rotate');
+    $('#view-rotate').click(function() {
+        changeBookState(3);
+    });
+
+    // Κλικ στο βιβλίο για εναλλαγή καταστάσεων
+    book.click(function() {
+        currentState = (currentState + 1) % states.length; // Εναλλαγή στην επόμενη κατάσταση
+        changeBookState(currentState);  // Αλλαγή κατάστασης και ενημέρωση κουμπιών
     });
 });
