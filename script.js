1f50b1d49354a65b3d38d4fd0c972e3379ba7a57
StathisDrim <asosproject@outlook.com>
2024-11-08 10:40:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-$(function(){
-	var book = $('#book');
-	$('#view-cover').click(function(){
-		$(this).addClass('cur').siblings().removeClass('cur');
-		book.removeClass().addClass('view-cover');
-	});
-	$('#view-back').click(function(){
-		$(this).addClass('cur').siblings().removeClass('cur');
-		book.removeClass().addClass('view-back');
-	});
-	$('#open-book').click(function(){
-		if ( book.attr('class') !='open-book') {
-			$(this).addClass('cur').siblings().removeClass('cur');
-			book.removeClass().addClass('open-book');
-		}else{
-			$(this).removeClass('cur');
-			$('#view-cover').addClass('cur');
-			book.removeClass().addClass('view-cover');
-		}
-	});
-	$('#view-rotate').click(function(){
-		$(this).addClass('cur').siblings().removeClass('cur');
-		book.removeClass().addClass('view-rotate');
-	});
+$(function() {
+    console.log("Το script.js φορτώθηκε σωστά!");
+    var book = $('#book');
+    
+    // View Cover
+    $('#view-cover').click(function(){
+        $(this).addClass('cur').siblings().removeClass('cur');
+        book.removeClass().addClass('view-cover');
+    });
+    
+    // View Back
+    $('#view-back').click(function(){
+        $(this).addClass('cur').siblings().removeClass('cur');
+        book.removeClass().addClass('view-back');
+    });
+    
+    // Open/Close Book
+    $('#open-book').click(function(){
+        if (book.attr('class') != 'open-book') {
+            $(this).addClass('cur').siblings().removeClass('cur');
+            book.removeClass().addClass('open-book');
+        } else {
+            $(this).removeClass('cur');
+            $('#view-cover').addClass('cur');
+            book.removeClass().addClass('view-cover');
+        }
+    });
+    
+    // 360° Showcase
+    $('#view-rotate').click(function(){
+        $(this).addClass('cur').siblings().removeClass('cur');
+        book.removeClass().addClass('view-rotate');
+    });
 });
