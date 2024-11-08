4b28b2eda4d732a2fce286dee9fcfe7655e3f238
StathisDrim <asosproject@outlook.com>
2024-11-08 10:48:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 $(function() {
     console.log("Το script.js φορτώθηκε σωστά!");
+
     var book = $('#book');
     
-    // View Cover
-    $('#view-cover').click(function(){
+    // Λίστα των καταστάσεων για εναλλαγή
+    var states = ["view-cover", "view-back", "open-book", "view-rotate"];
+    var currentState = 0;  // Ένδειξη τρέχουσας κατάστασης
+    
+    // Κλικ στα κουμπιά της κονσόλας
+    $('#view-cover').click(function() {
         $(this).addClass('cur').siblings().removeClass('cur');
         book.removeClass().addClass('view-cover');
+        currentState = 0;
     });
     
-    // View Back
-    $('#view-back').click(function(){
+    $('#view-back').click(function() {
         $(this).addClass('cur').siblings().removeClass('cur');
         book.removeClass().addClass('view-back');
+        currentState = 1;
     });
     
-    // Open/Close Book
-    $('#open-book').click(function(){
+    $('#open-book').click(function() {
         if (book.attr('class') != 'open-book') {
             $(this).addClass('cur').siblings().removeClass('cur');
             book.removeClass().addClass('open-book');
+            currentState = 2;
         } else {
             $(this).removeClass('cur');
             $('#view-cover').addClass('cur');
             book.removeClass().addClass('view-cover');
+            currentState = 0;
         }
     });
     
-    // 360° Showcase
-    $('#view-rotate').click(function(){
+    $('#view-rotate').click(function() {
         $(this).addClass('cur').siblings().removeClass('cur');
         book.removeClass().addClass('view-rotate');
+        currentState = 3;
+    });
+    
+    // Κλικ στο βιβλίο για εναλλαγή καταστάσεων
+    book.click(function() {
+        currentState = (currentState + 1) % states.length; // Επόμενη κατάσταση
+        book.removeClass().addClass(states[currentState]);  // Αλλαγή κατάστασης
     });
 });
