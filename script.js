18f3e2c16df03e36a5f0df832ebfcae7b0e08f20
StathisDrim <asosproject@outlook.com>
2024-11-08 11:47:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-$(document).ready(function() {
+$(function() {
     console.log("Το script.js φορτώθηκε σωστά!");
 
     var book = $('#book');
 
-    // Συνάρτηση για αλλαγή κατάστασης του βιβλίου
-    function setBookClass(className) {
-        book.removeClass().addClass(className);
-        console.log("Η κλάση του βιβλίου άλλαξε σε:", className);  // Έλεγχος αλλαγής κλάσης
+    // Καταστάσεις και κουμπιά που τις ελέγχουν
+    var states = ["view-cover", "view-back", "open-book", "view-rotate"];
+    var currentState = 0; // Ένδειξη της τρέχουσας κατάστασης
+
+    // Συνάρτηση για αλλαγή της κατάστασης του βιβλίου
+    function changeBookState(newStateIndex) {
+        currentState = newStateIndex;
+        book.removeClass().addClass(states[currentState]);
+        // Ενημέρωση των κουμπιών
+        $('#opt dd').removeClass('cur').eq(currentState).addClass('cur');
     }
 
-    // Χειρισμός κλικ για τα κουμπιά της κονσόλας
+    // Εναλλαγή με τα κουμπιά της κονσόλας
     $('#view-cover').click(function() {
-        setBookClass('view-cover');
+        changeBookState(0);
     });
 
     $('#view-back').click(function() {
-        setBookClass('view-back');
+        changeBookState(1);
     });
 
     $('#open-book').click(function() {
         if (book.hasClass('open-book')) {
-            setBookClass('view-cover');  // Κλείσιμο στο εξώφυλλο
+            changeBookState(0); // Κλείνει το βιβλίο και γυρνάει στο εξώφυλλο
         } else {
-            setBookClass('open-book');  // Άνοιγμα βιβλίου
+            changeBookState(2); // Ανοίγει το βιβλίο
         }
     });
 
     $('#view-rotate').click(function() {
-        setBookClass('view-rotate');
+        changeBookState(3);
     });
 
-    // Χειρισμός κλικ για το ίδιο το βιβλίο
+    // Κλικ στο βιβλίο για εναλλαγή καταστάσεων
     book.click(function() {
-        if (book.hasClass('view-cover')) {
-            setBookClass('view-back');
-        } else if (book.hasClass('view-back')) {
-            setBookClass('open-book');
-        } else if (book.hasClass('open-book')) {
-            setBookClass('view-rotate');
-        } else {
-            setBookClass('view-cover');
-        }
+        currentState = (currentState + 1) % states.length; // Εναλλαγή στην επόμενη κατάσταση
+        changeBookState(currentState);  // Αλλαγή κατάστασης και ενημέρωση κουμπιών
     });
 });
+
